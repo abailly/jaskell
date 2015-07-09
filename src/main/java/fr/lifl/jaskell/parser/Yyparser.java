ddaee2d667fd85ee7f7b5740040e09caad3d1f71
Arnaud Bailly <arnaud.oqube@gmail.com>
2015-07-09 09:36:30 +0200

    cleanup copyright

--- src/main/java/fr/lifl/jaskell/parser/Yyparser.java
+++ src/main/java/fr/lifl/jaskell/parser/Yyparser.java
@@ -1,8 +1,8 @@
 /**
- *  Copyright Murex S.A.S., 2003-2013. All Rights Reserved.
+ * Copyright Arnaud Bailly, 2003-2013. All Rights Reserved.
  * 
- *  This software program is proprietary and confidential to Murex S.A.S and its affiliates ("Murex") and, without limiting the generality of the foregoing reservation of rights, shall not be accessed, used, reproduced or distributed without the
- *  express prior written consent of Murex and subject to the applicable Murex licensing terms. Any modification or removal of this copyright notice is expressly prohibited.
+ 
+ *
  */
 //### This file created by BYACC 1.8(/Java extension  1.1)
 //### Java capabilities added 7 Jan 97, Bob Jamison
